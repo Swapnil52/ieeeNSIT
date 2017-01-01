1b85308f3e50bf12924083c236f91f752d41a53b
Swapnil Dhanwal <swapnil@swapnils-macbook-pro-2.local>
2017-01-01 13:38:02 +0530

    Added pull to refresh in the events module

--- ieeeNSIT/eventsViewController.swift
+++ ieeeNSIT/eventsViewController.swift
@@ -24,6 +24,8 @@ class eventsViewController : UIViewController {
     var events = [[String : AnyObject]]()
     var spinner = UIActivityIndicatorView()
     var retryButton = UIButton()
+    var refreshView = UIView()
+    var refreshImageView = UIImageView()
     
     override func viewDidLoad() {
         
@@ -33,6 +35,14 @@ class eventsViewController : UIViewController {
         index = 0;
         swipeEnabled = false
         events.removeAll()
+        
+        //setting up the refresher view
+        refreshView = UIView(frame: CGRect(x: self.view.bounds.width/2 - self.view.bounds.width*0.20/2, y: self.mainView.frame.maxY+self.view.bounds.width*0.20/2, width: self.view.bounds.width*0.20, height: self.view.bounds.width*0.20))
+        refreshImageView = UIImageView(frame: self.refreshView.bounds)
+        refreshImageView.image = UIImage(named: "eventsRefresher.png")
+        self.refreshView.addSubview(self.refreshImageView)
+        refreshView.alpha = 0
+        self.view.addSubview(refreshView)
         
         showMainView()
         spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
@@ -67,6 +77,7 @@ class eventsViewController : UIViewController {
     
     func showMainView()
     {
+        
         //setting up the main view. Add subviews to this view as desired.
         mainView.layer.cornerRadius = 10
         mainView.frame = CGRect(x: self.view.bounds.width/2-self.view.bounds.width*0.90/2, y: self.view.bounds.height/2-self.view.bounds.height*0.90/2, width: self.view.bounds.width*0.85, height: self.view.bounds.height*0.85)
@@ -74,7 +85,7 @@ class eventsViewController : UIViewController {
         self.view.addSubview(mainView)
         
         //setting up the blur view
-        let blur = UIBlurEffect(style: .regular)
+        let blur = UIBlurEffect(style: .light)
         blurView = UIVisualEffectView(effect: blur)
         blurView.frame = self.mainView.bounds
         self.mainView.addSubview(blurView)
@@ -97,8 +108,8 @@ class eventsViewController : UIViewController {
             }
         }
         
-        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.mainView.bounds.width, height: self.mainView.bounds.height * 0.4))
-        imageView.backgroundColor = getColor(red: 37, green: 55, blue: 50)
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.mainView.bounds.width, height: self.mainView.bounds.height * 0.3))
+        imageView.backgroundColor = getColor(red: 68, green: 74, blue: 236)
         imageView.clipsToBounds = true
         self.mainView.addSubview(imageView)
         
@@ -106,17 +117,30 @@ class eventsViewController : UIViewController {
         lineLabel.backgroundColor = getColor(red: 37, green: 50, blue: 55)
         self.mainView.addSubview(lineLabel)
         
-        nameLabel = UILabel(frame: CGRect(x: 10, y: self.lineLabel.frame.maxY + 20, width: self.mainView.frame.width * 0.35, height: 30))
-        nameLabel.text = "\(self.events[self.index]["event_name"])"
+        nameLabel = UILabel(frame: CGRect(x: 10, y: self.lineLabel.frame.maxY + 10, width: self.mainView.frame.width * 0.35, height: 30))
+        if let f = UIFont(name: "Avenir Book", size: 13)
+        {
+            let s = NSMutableAttributedString(string: "\(self.events[self.index]["event_name"]!)", attributes: [NSFontAttributeName : f])
+            self.nameLabel.attributedText = s
+        }
         self.mainView.addSubview(nameLabel)
         
-        dateLabel = UILabel(frame: CGRect(x: 0.65*self.mainView.bounds.width - 10, y: self.lineLabel.frame.maxY, width: self.mainView.bounds.width * 0.35, height: 30))
-        dateLabel.text = "\(self.events[self.index]["date"])"
+        dateLabel = UILabel(frame: CGRect(x: 0.65*self.mainView.bounds.width - 10, y: self.lineLabel.frame.maxY+10, width: self.mainView.bounds.width * 0.35, height: 30))
+        if let f = UIFont(name: "Avenir Book", size: 13)
+        {
+            let s = NSMutableAttributedString(string: "\(self.events[self.index]["date"]!)", attributes: [NSFontAttributeName : f])
+            self.dateLabel.attributedText = s
+        }
         self.mainView.addSubview(dateLabel)
         
         textView = UITextView(frame: CGRect(x: 10, y: self.nameLabel.frame.maxY + 20, width: self.mainView.bounds.width - 20, height: self.mainView.bounds.height - (self.nameLabel.frame.maxY + 40)))
-        textView.text = "\(self.events[self.index]["description"])"
+        if let f = UIFont(name: "Avenir Book", size: 15)
+        {
+            let s = NSMutableAttributedString(string: "\(self.events[self.index]["description"]!)", attributes: [NSFontAttributeName : f])
+            self.textView.attributedText = s
+        }
         textView.isEditable = false
+        textView.dataDetectorTypes = .all
         self.mainView.addSubview(textView)
         
         completionHandler()
@@ -151,12 +175,45 @@ class eventsViewController : UIViewController {
     
     func refresh()
     {
-        self.viewDidLoad()
+        index = 0;
+        swipeEnabled = false
+        events.removeAll()
+        
+        showMainView()
+        spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        spinner.frame = CGRect(x: self.mainView.bounds.width/2 - 50, y: self.mainView.bounds.height/2 - 50, width: 100, height: 100)
+        spinner.hidesWhenStopped = true
+        self.mainView.addSubview(spinner)
+        spinner.startAnimating()
+        
+        downloadEvents { (success, events, count) in
+            
+            if success == true
+            {
+                self.count = count
+                self.events = events
+                self.spinner.stopAnimating()
+                self.displayEvent(self.index, completionHandler: {
+                    
+                    self.swipeEnabled = true
+                    
+                })
+            }
+                
+            else
+            {
+                self.displayError()
+                self.spinner.stopAnimating()
+            }
+            
+        }
+
     }
     
     func downloadEvents(completionHandler : @escaping (_ success : Bool, _ array : [[String:AnyObject]], _ count : Int) -> Void)
     {
-        let url = URL(string: "http://ieeensit.org/appevents.json")
+        //let url = URL(string: "http://ieeensit.org/appevents.json")
+        let url = URL(string: "https://api.myjson.com/bins/pz64v")
         let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
             
             if let error = error
@@ -179,20 +236,23 @@ class eventsViewController : UIViewController {
                         
                         do
                         {
-
-                            let jsonData = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                            let jsonData = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary
                             
                             if let jsonData = jsonData as? [String : AnyObject]
                             {
                                 
-                                if let total = jsonData["Total"] as? Int
+                                if let total = jsonData["Total"] as? String
                                 {
                                     
                                     if let array = jsonData["events"] as? [[String : AnyObject]]
                                     {
-                                         completionHandler(true, array, total)
+                                         completionHandler(true, array, Int(total)!)
                                     }
                                     
+                                }
+                                else
+                                {
+                                    completionHandler(false, [], 0)
                                 }
                                 
                             }
@@ -215,24 +275,42 @@ class eventsViewController : UIViewController {
     func drag(gesture : UIPanGestureRecognizer)
     {
         let translation = gesture.translation(in: self.view)
-        self.mainView.layer.setAffineTransform(CGAffineTransform(translationX: translation.x, y: translation.y))
-        if gesture.state == .began
+        let velocity = gesture.velocity(in: self.view)
+        let transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        self.mainView.layer.setAffineTransform(transform)
+        
+        //rotate the refresher image view if dragging downwards
+        if translation.y > 0 && velocity.y > 0
         {
-            UIView.animate(withDuration: 0.3, delay: 0, options: .allowUserInteraction, animations: { 
-                
-                self.mainView.layer.setAffineTransform(CGAffineTransform(scaleX: 1.01, y: 1.01))
-                
-                }, completion: nil)
+            let alphaScale = translation.y/100
+            self.refreshView.alpha = alphaScale
+            if (1*translation.y/30*3.14/180 < 0.2)
+            {
+                let transform = self.refreshView.layer.affineTransform()
+                let rotate = CGAffineTransform(rotationAngle: 1*translation.y/30*3.14/180)
+                let t = (transform.concatenating(rotate))
+                self.refreshView.layer.setAffineTransform(t)
+            }
         }
-        else if gesture.state == .ended
+        
+        if gesture.state == .ended
         {
             let x = translation.x
-            print(x)
+            
+            //get rid of the refresh view if the user has swiped far anough to initiate the page swipe animation
+            if (1*translation.y/30*3.14/180 > 0.1)
+            {
+                self.refresh()
+            }
+            
+            //initiate the page swipe animation if the user has swiped far enough
             if x >= -self.view.bounds.width*0.25 && x <= self.view.bounds.width*0.25
             {
-                UIView.animate(withDuration: 0.4, delay: 0, options: .allowUserInteraction, animations: { 
+                UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: .allowUserInteraction, animations: {
                     
                     self.mainView.layer.setAffineTransform(CGAffineTransform.identity)
+                    self.refreshView.alpha = 0
+                    self.refreshView.layer.setAffineTransform(CGAffineTransform.identity)
                     
                     }, completion: nil)
             }
@@ -240,23 +318,28 @@ class eventsViewController : UIViewController {
             {
                 if (x < self.view.bounds.width*0.25)
                 {
-                    swipeLeft()
+                    swipeLeft(gesture)
                 }
                 else
                 {
-                    swipeRight()
+                    swipeRight(gesture)
                 }
             }
         }
     }
     
-    func swipeLeft()
+    func swipeLeft(_ gesture : UIPanGestureRecognizer)
     {
+        
         if self.index < self.count-1 && self.swipeEnabled == true
         {
-            UIView.animate(withDuration: 0.4, delay: 0, options: .allowUserInteraction, animations: {
+            
+            UIView.animate(withDuration: 0.4, animations: {
                 
-                self.mainView.layer.transform = CATransform3DMakeTranslation(-1000, 0, 0)
+                let transform = CGAffineTransform(translationX: -1000, y: 0)
+                self.mainView.layer.setAffineTransform(transform)
+                self.refreshView.alpha = 0
+                self.refreshView.layer.setAffineTransform(CGAffineTransform.identity)
                 
             }) { (success) in
                 
@@ -267,7 +350,7 @@ class eventsViewController : UIViewController {
                     
                 })
                 
-                UIView.animate(withDuration: 0.4, delay: 0, options: .allowUserInteraction, animations: {
+                UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: .allowUserInteraction, animations: {
                     
                     self.mainView.layer.transform = CATransform3DIdentity
                     
@@ -282,22 +365,28 @@ class eventsViewController : UIViewController {
         }
         else
         {
-            UIView.animate(withDuration: 0.4, animations: {
+            UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: .allowUserInteraction,  animations: {
                 
                 self.mainView.layer.setAffineTransform(CGAffineTransform.identity)
+                self.refreshView.alpha = 0
+                self.refreshView.layer.setAffineTransform(CGAffineTransform.identity)
                 
                 }, completion: nil)
 
         }
     }
     
-    func swipeRight()
+    func swipeRight(_ gesture : UIPanGestureRecognizer)
     {
         if self.index > 0 && self.swipeEnabled == true
         {
             UIView.animate(withDuration: 0.4, animations: {
                 
-                self.mainView.layer.transform = CATransform3DMakeTranslation(+1000, 0, 0)
+                let transform = CGAffineTransform(translationX: 1000, y: 0)
+                self.mainView.layer.setAffineTransform(transform)
+                self.refreshView.alpha = 0
+                self.refreshView.layer.setAffineTransform(CGAffineTransform.identity)
+
                 
             }) { (success) in
                 
@@ -308,7 +397,7 @@ class eventsViewController : UIViewController {
                     
                 })
                 
-                UIView.animate(withDuration: 0.4, delay: 0, options: .allowUserInteraction, animations: {
+                UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: .allowUserInteraction, animations: {
                     
                     self.mainView.layer.transform = CATransform3DIdentity
                     
@@ -320,9 +409,11 @@ class eventsViewController : UIViewController {
         }
         else
         {
-            UIView.animate(withDuration: 0.4, animations: {
+            UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: .allowUserInteraction,  animations: {
                 
                 self.mainView.layer.setAffineTransform(CGAffineTransform.identity)
+                self.refreshView.alpha = 0
+                self.refreshView.layer.setAffineTransform(CGAffineTransform.identity)
                 
                 }, completion: nil)
 
